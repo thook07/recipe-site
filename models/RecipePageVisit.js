cf0b27d53052f13e79847956145e1f056277387f
thook07 <thook07@gmail.com>
2020-09-20 13:07:27 -0700

    [Enhancement 02] Update admin/recipe-views with a well formatted date

--- models/RecipePageVisit.js
+++ models/RecipePageVisit.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('../config/database');
+const dateformat = require('dateformat');
 
 const RecipePageVisit = db.define('recipePageVisit', {
     recipeId: {
@@ -7,6 +8,24 @@ const RecipePageVisit = db.define('recipePageVisit', {
     },
     userId: {
         type: Sequelize.INTEGER
+    },
+    createdAtPretty: {
+        type: Sequelize.VIRTUAL,
+        get() {
+            return dateformat(this.get('createdAt'), "dddd, mm/dd/yy h:MM:ss TT Z");
+        },
+        set(value) {
+            throw new Error('Do not try to set the `url` value! User attLink instead.');
+        }
+    },
+    updatedAtPretty: {
+        type: Sequelize.VIRTUAL,
+        get() {
+            return dateformat(this.get('updatedAt'), "dddd, mm/dd/yy h:MM:ss TT Z");
+        },
+        set(value) {
+            throw new Error('Do not try to set the `url` value! User attLink instead.');
+        }
     }
 
 });
